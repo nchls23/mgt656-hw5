ce3d231c47944a8a34f75ec248eed0aa223064d1
Nicholas D'Andre <nicholasmdandre@gmail.com>
2016-10-05 17:17:18 +0000

    Add nickname route

--- server.js
+++ server.js
@@ -2,7 +2,13 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(request, response){
-    response.send('Hello MGT-656!');
+    response.send('Hello MGT-656!  I am a complete ninja now');
 });
 
-app.listen(process.env.PORT || 4000);
+//you are making the code here respond to your class nickname
+app.get('/nickname', function(request, response){
+    response.send('cloudy-cat');
+});
+
+app.listen(process.env.PORT || 4000,
+process.env.IP || 'localhost');
